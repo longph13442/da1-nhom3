45d41b8f9293fa00041f7eba3e13a4a7ccca6046
huybtph14352 <huybtph14352@fpt.edu.vn>
2021-11-11 22:46:46 +0700

    Up Trang Tài Khoản

---
+++ client/business/PageAccount/editaddress.php
@@ -0,0 +1,47 @@
+<link rel="stylesheet" href="../../Css/editaddress.css">
+<div class="editaddress">
+    <div class="title-address">
+        <h3>Địa Chỉ Thanh Toán</h3>
+    </div>
+    <form action="">
+        <div class="form-group">
+            <label for="">Họ Và Tên</label>
+            <br>
+            <input type="text" name="">
+        </div>
+        <div class="form-group">
+            <label for="">Địa Chỉ Nhận Hàng</label>
+            <br>
+            <input type="text" name="">
+        </div>
+        <div class="form-group">
+            <label for="">Tỉnh / Thành Phố</label>
+            <select name="calc_shipping_provinces" required="">
+                <option value="">Tỉnh / Thành phố</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="">Quận/Huyện</label>
+            <br>
+            <select name="calc_shipping_district" required="">
+                <option value="">Quận / Huyện</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="">Phường/Xã</label>
+            <br>
+            <select name="calc_shipping_ward" required="">
+                <option value="">Phường / Xã</option>
+            </select>
+
+        </div>
+        <div class="savelocation">
+            <button type="submit" name="btnsvlocation">Lưu Địa Chỉ</button>
+
+        </div>
+    </form>
+</div>
+<!-- Jquery -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+<script src='https://cdn.jsdelivr.net/gh/vietblogdao/js/districts.min.js'></script>
+<script src="../../JS/location.js"></script>
---
+++ client/business/PageAccount/info.php
@@ -0,0 +1,34 @@
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="../../Css/info.css">
+<div class="info">
+    <div class="categorys">
+        <nav class="menu_info">
+            <ul>
+                <li><a name="" href="?info">
+                        <ion-icon name="person-circle-outline"></ion-icon> Thông Tin Tài Khoản
+                    </a></li>
+                <li><a name="" href="?address">
+                        <ion-icon name="location-outline"></ion-icon>Địa Chỉ
+                    </a></li>
+                <li><a name="" href="?cart">
+                        <ion-icon name="cart-outline"></ion-icon>Đơn Hàng
+                    </a></li>
+                <li><a name="" href="?resestpass">
+                        <ion-icon name="refresh-circle-outline"></ion-icon>Đổi Mật Khẩu
+                    </a></li>
+                <li><a name="" href="?logout">
+                        <ion-icon name="log-out-outline"></ion-icon>Đăng Xuất
+                    </a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="info-detail">
+        <?php
+        include_once  $VIEW;
+        ?>
+    </div>
+</div>
