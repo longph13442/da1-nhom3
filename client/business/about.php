e1f46611c5b3af83c8ef2d527caf71601dfa9bdb
huybtph14352 <huybtph14352@fpt.edu.vn>
2021-11-11 22:35:00 +0700

    cập nhật trang giới thiệu

--- client/business/about.php
+++ client/business/about.php
@@ -1,4 +1,156 @@
-<?php 
-function about(){
-    echo "đây là giới thiệu abc!";
-}
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+<!--link css-->
+<link rel="stylesheet" href="../../Css/Gioithieu.css">
+<div class="introduce">
+    <div class="intro_header">
+        <div class="intro_header_img">
+            <img src="../../Image/image 1.png" alt="">
+        </div>
+        <div class="intro_header_text">
+            <div class="intro_header_title">
+                <p>Shop mỹ phẩm của chúng tôi</p>
+            </div>
+            <div class="intro_header_content">
+                <p> chúng tôi cam kết luôn mang lại những sản phẩm chính hãng với giá tốt nhất cho khách hàng. Những sản
+                    phẩm bán trên Kosmebox đều được bảo đảm cung cấp bởi những thương hiệu, nhà cung cấp uy tín mà chúng
+                    tôi đã cẩn trọng đánh giá, dựa trên các tiêu chí của khu vực về an toàn sản phẩm, điều kiện sản xuất
+                    và lưu trữ bảo quản. </p>
+            </div>
+        </div>
+    </div>
+    <div class="main_introduce">
+        <div class="main_introduce_story">
+            <div class="main_introduce_img">
+                <img src="../../Image/image 6.png" alt="">
+            </div>
+            <div class="main_introduce_text">
+                <div class="main_introduce_title">
+                    <h3>Câu Chuyện</h3>
+                    <p><a href="">Hãy là người đẹp của riêng bạn</a></p>
+                </div>
+                <div class="story_content">
+                    <p class="contentsr">
+                        Lớp nền tự nhiên hoàn thiện làn da được làm giàu với siêu trái cây chống oxy hóa, vitamin và các
+                        chất dinh dưỡng nuôi dưỡng da khác. Công thức dạng kem lỏng với lớp nền mờ nguyên sơ cho làn da
+                        mềm mại, mịn màng như nhung. Được làm bằng nguyên liệu sạch, không độc hại, các sản phẩm của
+                        chúng tôi được thiết kế cho tất cả mọi người…
+                    </p>
+                    <button class="readmore" name="readmore">Đọc Thêm</button>
+                </div>
+            </div>
+        </div>
+        <div class="main_introduce_info">
+            <div class="address info-flex">
+                <div class="address_img">
+                    <img src="../../Image/image 2.png" alt="">
+                </div>
+                <div class="address_text info-text">
+                    <h3>Địa Chỉ</h3>
+                    <div class="address_dt">
+                        <p>
+                            Số 1 Trịnh Văn Bô,Nam Từ Liêm, Hà Nội
+                        </p>
+                        <p>
+                            Số 5 Trịnh Văn Bô,Nam Từ Liêm, Hà Nội
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="contact info-flex">
+                <div class="img_contact">
+                    <img src="../../Image/image 3.png" alt="">
+                </div>
+                <div class="contact_text info-text">
+                    <h3>Liên Hệ</h3>
+                    <div class=" contact_dt ">
+                        <p>
+                            Điện Thoại: 0987654321
+                        </p>
+                        <p>
+                            Hotline: 0987654321
+                        </p>
+                        <p>
+                            Email: fpt@fpt.edu.vn
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class=" openhour info-flex">
+                <div class="openhour_img">
+                    <img src="../../Image/image 4.png" alt="">
+                </div>
+                <div class="openhour_contact info-text">
+                    <h3>Giờ Mở Cửa</h3>
+                    <div class="openhour_text">
+                        <p>
+                            Thứ 2 - thứ 6: 08:30 - 20:00
+                        </p>
+                        <p>
+                            Thứ 7 & Chủ nhật: 09:30 - 21:30
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class=" intro_product">
+            <div class="intro_title_product">
+                <h2>SẢN PHẨM LÀM ĐẸP TỰ NHIÊN & HỮU CƠ</h2>
+            </div>
+            <div class="image-slider">
+                <div class="it_product_img">
+                    <img src="../../Image/image 9.png" alt="">
+                </div>
+                <div class="it_product_img">
+                    <img src="https://images.unsplash.com/photo-1598440947619-2c35fc9aa908?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29zbWV0aWNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="">
+                </div>
+                <div class="it_product_img">
+                    <img src="https://images.unsplash.com/photo-1573575155376-b5010099301b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvc21ldGljc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="">
+                </div>
+                <div class="it_product_img">
+                    <img src="../../Image/image 11.png" alt="">
+                </div>
+                <div class="it_product_img">
+                    <img src="../../Image/image 12.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="discover">
+            <div class="discover_title">
+                <h3>
+                    Khám Phá Thêm
+                </h3>
+            </div>
+            <div class="discoverif">
+                <div class="discover_more">
+                    <div class="discover_img">
+                        <img src="../../Image/image 7.png" alt="">
+                    </div>
+                    <div class="discover_text">
+                        <p><a href="">Tìm 1 cửa hàng</a></p>
+                        <span>
+                            <a href="">Xem Thêm</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="discover_more">
+                    <div class="discover_img">
+                        <img src="../../Image/image 8.png" alt="">
+                    </div>
+                    <div class="discover_text">
+                        <p><a href="">Thông tin thêm</a></p>
+                        <span>
+                            <a href="">Xem Thêm</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--jquery-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="../../JS/slickslide.js"></script>
