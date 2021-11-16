689b5bc663e6ccb694ec018929ff63b13d8c0c1b
trung9901 <79823094+trung9901@users.noreply.github.com>
2021-11-16 21:53:29 +0700

    update chi tiet sp

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+session_start();
 $url = isset($_GET["url"]) ? $_GET["url"] : "/";
-
+include "./dao/pdo.php";
 switch ($url) {
     case '/':
         include "./client/business/homepage.php";
@@ -8,6 +9,7 @@ switch ($url) {
     case 'product':
         include "./client/business/product.php";
         break;
+    
     case 'about':
         include "./client/business/about.php";
         
@@ -17,6 +19,15 @@ switch ($url) {
         break;
     case 'dashboard':
         include "./client/dashboard/product.php";
+        break;
+    case 'product_detail':
+        if (isset($_GET['ma_sp']) && ($_GET['ma_sp'] > 0)) {
+            $info = san_pham_select_by_id($_GET['ma_sp']);
+            extract($info);
+            $info2 = san_pham_select_by_loai_chitiet($ma_loai, $_GET['ma_sp']);
+            include "./client/business/product_details.php";
+        } 
+        // include "./client/business/product_details.php";
         break;
     default:
         # code...
