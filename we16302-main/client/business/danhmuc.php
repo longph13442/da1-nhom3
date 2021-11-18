3ffd641d4e45b4e11b6df786ee063d963e2bdbb3
Thetam007 <79209922+Thetam007@users.noreply.github.com>
2021-11-19 00:28:38 +0700

    Delete we16302-main directory

--- we16302-main/client/business/danhmuc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once './dao/system_dao.php';
-require_once './dao/danhmuc.php';
-function loadall_dm_sp()
-{
- $dmsp=  loadall_dm();
-    client_render('products/index.php', compact('dmsp'));
-}
-?>
