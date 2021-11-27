0b30e30cae32a3bf41e69c703fcf58af8885a154
longtvph13442 <longtvph13442@fpt.edu.vn>
2021-11-27 15:21:10 +0700

    long-up-bolg

---
+++ client/business/about.php
@@ -0,0 +1,5 @@
+<?php 
+function about (){
+    client_render('about/about.php');
+}
+?>
---
+++ client/views/about/about.php
@@ -0,0 +1,100 @@
+<div class="blog " style="margin:0 100px;">
+        <div class="row ">
+            <div class="text-center mt-3">
+                <img src="<?=ADMIN_ASSET?>Image/flower.png" alt="" width="10%">
+            </div>
+            <!-- img -->
+            <h4 class="text-center fw-bold">VỀ SUNFLOWER - SHOP MỸ PHẨM CỦA CHÚNG TÔI </h4>
+            <!-- title -->
+            <p class="mx-auto my-3 " style="width: 90%;"> Chúng tôi cam kết luôn mang lại những sản phẩm chính hãng với
+                giá tốt nhất cho khách hàng. Những sản phẩm bán trên Kosmebox đều được bảo đảm cung cấp bởi những thương
+                hiệu, nhà cung cấp uy tín mà chúng tôi đã cẩn trọng đánh giá, dựa trên các tiêu chí của khu vực về an
+                toàn sản phẩm, điều kiện sản xuất và lưu trữ bảo quản. </p>
+
+        </div>
+        <!-- row -->
+        <div class="row my-3">
+            <div class="col-5 ">
+                <img src="<?=ADMIN_ASSET?>Image/ab.jpg" alt="" width="100%">
+            </div>
+            <div class="col-7">
+                <p class="fw-bold">Câu chuyện</p>
+                <p>Với mục tiêu mang đến cơ hội trải nghiệm các sản phẩm làm đẹp chất lượng tốt – giá tốt nhất thị
+                    trường, góp phần đắp đầy những vẻ đẹp khuyết thiếu, truyền cảm hứng dùng mỹ phẩm và giúp vẻ đẹpViệt
+                    tỏa sáng, Beauty Garden luôn không ngừng phấn đấu để hoàn thiện chất lượng dịch vụ của chính mình.
+                </p>
+                <button class="btn btn-warning">Xem thêm </button>
+            </div>
+        </div>
+        <!-- row -->
+        <div class="row my-5">
+            <div class="col-4 text-center">
+                <span class="material-icons-outlined">
+                    location_on
+                </span>
+                <p class="">Địa chỉ: Số 2, Trịnh Văn Bô, Hà nội
+                </p>
+                <p class="">Địa chỉ: 255 Bình Lợi, P. 13, Q. Bình Thạnh. Tp.HCM
+                </p>
+                <p class="">Địa chỉ: 587 Bình Lợi, P. 13, Q. Bình Thạnh. Tp.HÀ Nội
+                </p>
+            </div>
+            <!-- 1 -->
+            <div class="col-4 text-center">
+                <span class="material-icons-outlined">
+                    phone_in_talk
+                </span>
+                <p>Tel: 02 873 000 283 -
+                </p>
+                <p>Email: info@beautygarden.vn</p>
+                <p> Website: www.beautygarden.vn</p>
+            </div>
+            <!-- 2 -->
+            <div class="col-4 text-center">
+                <span class="material-icons-outlined">
+                    schedule
+                </span>
+                <p>Thứ 2 - Thứ 6 : 9h00 - 22h00</p>
+                <p>Thứ 7 - Chủ nhật : 9h00 - 12h00</p>
+            </div>
+            <!-- 3 -->
+
+        </div>
+        <!-- row -->
+        <div class="title my-3">
+            <h5 class="fw-bold">CHÚNG TÔI CÓ</h5>
+        </div>
+        <!-- end title -->
+        <div class="row">
+            <div class="col-12 text-center">
+                <iframe width="830" height="450" src="https://www.youtube.com/embed/Glqn9l-Smnw"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+        <!-- row -->
+        <div class="title my-3">
+            <h5 class="fw-bold">ĐỊA ĐIỂM CỦA CHÚNG TÔI</h5>
+        </div>
+        <!-- end title -->
+        <div class="row mb-5">
+            <div class="col-3">
+                <div class="list-group ">
+                    <a href="#" class="list-group-item list-group-item-action py-4 bg-warning" aria-current="true">
+                      Địa chỉ cửa hàng
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action py-4">Địa chỉ: Số 2, Trịnh Văn Bô, Hà nội</a>
+                    <a href="#" class="list-group-item list-group-item-action py-4">Địa chỉ: 255 Bình Lợi, P. 13, Q. Bình Thạnh. Tp.HCM</a>
+                    <a href="#" class="list-group-item list-group-item-action py-4">Địa chỉ: 587 Bình Lợi, P. 13, Q. Bình Thạnh. Tp.HÀ Nội</a>
+                    <a href="#" class="list-group-item list-group-item-action py-4 border-bottom-0" >Địa điểm của bạn</a>
+                    
+                  </div>
+            </div>
+            <div class="col-9">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.863855881391!2d105.74459841415757!3d21.038132792835867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1637986728873!5m2!1svi!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+           
+        </div>
+    </div>
+    <!-- end blog -->
