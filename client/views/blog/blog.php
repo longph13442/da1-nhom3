b96cfe8be8d3cb495a77ff29316521f3e355c4f6
longtvph13442 <longtvph13442@fpt.edu.vn>
2021-11-28 15:01:55 +0700

    long-update-blog

--- client/views/blog/blog.php
+++ client/views/blog/blog.php
@@ -51,7 +51,7 @@
         </div>
 
         <div class="row g-5">
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <h3 class="pb-4 mb-4  border-bottom">
                     CÁC SẢN PHẨM MAKEUP 2 TRONG 1 ĐA ZI NĂNG CHO NÀNG
                 </h3>
@@ -177,7 +177,7 @@
 
             </div>
 
-            <div class="col-md-4 ">
+            <div class="col-md-3 ">
                 <div class="position-sticky" style="top: 2rem;">
                     <div class="p-4 mb-3 bg-light rounded">
                         <h4 class="fst-italic">Chủ đề</h4>
