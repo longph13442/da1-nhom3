9a19901534e338a5cb4c635deda9a702614e14c2
longtvph13442 <longtvph13442@fpt.edu.vn>
2021-12-02 07:40:09 +0700

    long-update-header

--- client/views/layouts/header.php
+++ client/views/layouts/header.php
@@ -4,14 +4,14 @@
             <a href="<?= ROOT_URL ?>" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
                 <img src="<?= ADMIN_ASSET ?>image/logo.png" alt="" height="60">
             </a>
-
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="<?= ROOT_URL ?>" class="nav-link px-2 link-secondary">Trang chủ</a></li>
-                <li><a href="<?= ROOT_URL ?>danh-muc" class="nav-link px-2 link-dark">Sản Phẩm</a></li>
-                <li><a href="<?= ROOT_URL ?>about" class="nav-link px-2 link-dark">Giới thiệu</a></li>
-                <li><a href="<?= ROOT_URL ?>blog" class="nav-link px-2 link-dark">Blog</a></li>
-            </ul>
-
+            <nav class=" stroke nav col-12 col-lg-auto me-lg-auto ">
+                <ul class="justify-content-center mb-md-0" >
+                    <li><a href="<?= ROOT_URL ?>" class="nav-link px-2 link-secondary">Trang chủ</a></li>
+                    <li><a href="<?= ROOT_URL ?>danh-muc" class="nav-link px-2 link-dark">Sản Phẩm</a></li>
+                    <li><a href="<?= ROOT_URL ?>about" class="nav-link px-2 link-dark">Giới thiệu</a></li>
+                    <li><a href="<?= ROOT_URL ?>blog" class="nav-link px-2 link-dark">Blog</a></li>
+                </ul>
+            </nav>
             <form class="col-12 col-xl-5 col-lg-5 col-md-4 mb-3 mb-lg-0 me-lg-5 " action="<?= ROOT_URL ?>tim-kiem" method="post">
                 <input type="search" class="form-control" placeholder="Search..." aria-label="Search" name="kyw">
             </form>
