b50a8b4c73ce1bed8e2a99262de3f70a7a275f28
huybtph14352 <huybtph14352@fpt.edu.vn>
2021-12-03 16:31:13 +0700

    Huy - up number format

--- client/views/products/index.php
+++ client/views/products/index.php
@@ -56,10 +56,10 @@
 
                            <div class="col-12 col-sm-12 col-lg-4 col-xl-4 text-center  ">
                                <a href="<?= ROOT_URL ?>sanphamct&ma_sp=<?= $p['ma_sp'] ?>"><img src="<?= ADMIN_ASSET ?>image/<?= $p['anh_sp'] ?>  " alt="" class="img-fluid" height="50"></a>
-     <a href="index.php?url=sanphamct&ma_sp=<?= $p['ma_sp'] ?>" class="nav-link text-success">
-                                <p class="text-center"> <?= $p['ten_sp'] ?> </p>
-                            </a>
-                               <p class=" display-8 text-center fw-bold text-secondary mt-4"> <?= $p['giatien'] ?> </p>
+                               <a href="index.php?url=sanphamct&ma_sp=<?= $p['ma_sp'] ?>" class="nav-link text-success">
+                                   <p class="text-center"> <?= $p['ten_sp'] ?> </p>
+                               </a>
+                               <p class=" display-8 text-center fw-bold text-secondary mt-4"> <?= number_format($p['giatien'], 0) ?>VNĐ </p>
 
                                <div class="star mb-md-4">
                                    <i class="fas fa-star "></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
@@ -118,9 +118,9 @@
            <!-- end row -->
            <h4 class="my-5">NHỮNG SẢN PHẨM ƯU ĐÃI</h4>
 
-                 <div class="row">
-  <?php foreach ($dskm as $p) : ?>
-<!-- 
+           <div class="row">
+               <?php foreach ($dskm as $p) : ?>
+                   <!-- 
                   <div class="row">
                <div class="col-12 col-sm-12 col-lg-4 col-xl-4 text-center  ">
                                <a href="<?= ROOT_URL ?>sanphamct&ma_sp=<?= $p['ma_sp'] ?>"><img src="<?= ADMIN_ASSET ?>image/ " alt="" class="img-fluid" height="50"></a>
@@ -138,33 +138,33 @@
 
 
            </div> -->
-         
 
-               <div class="col-6 col-sm-6 col-lg-3 col-xl-3 text-center  " >
-             <a href="<?= ROOT_URL ?>sanphamct&ma_sp=<?= $p['ma_sp'] ?>">      <img src="<?= homepase_ASSET ?>image/<?= $p['anh_sp'] ?> " alt="" class="img-fluid" height="50"></a>
 
-                   <a href="index.php?url=sanphamct&ma_sp=<?= $p['ma_sp'] ?>" class="nav-link text-success">
-                                <p class="text-center"> <?= $p['ten_sp'] ?> </p>
-                            </a>
-                   <p class=" display-8 text-center fw-bold text-secondary mt-4"><?= $p['giatien'] ?>đ - <?= $p['giamgia'] ?>đ </p>
-                   <div class="star mb-md-4">
-                       <i class="fas fa-star "></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
+                   <div class="col-6 col-sm-6 col-lg-3 col-xl-3 text-center  ">
+                       <a href="<?= ROOT_URL ?>sanphamct&ma_sp=<?= $p['ma_sp'] ?>"> <img src="<?= homepase_ASSET ?>image/<?= $p['anh_sp'] ?> " alt="" class="img-fluid" height="50"></a>
+
+                       <a href="index.php?url=sanphamct&ma_sp=<?= $p['ma_sp'] ?>" class="nav-link text-success">
+                           <p class="text-center"> <?= $p['ten_sp'] ?> </p>
+                       </a>
+                       <p class=" display-8 text-center fw-bold text-secondary mt-4"><?= $p['giatien'] ?>đ - <?= $p['giamgia'] ?>đ </p>
+                       <div class="star mb-md-4">
+                           <i class="fas fa-star "></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
+                       </div>
                    </div>
-               </div>
-       
 
-   
 
- <?php endforeach ?>
-         </div>
 
-         
+
+               <?php endforeach ?>
+           </div>
+
+
            <!-- end row -->
-    
+
            <!-- end row -->
        </div>
 
 
 
-       
+
    </main>
