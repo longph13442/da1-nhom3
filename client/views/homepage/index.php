2945485a12d3d6a7b589b4c3c0b46d3001e231c3
huybtph14352 <huybtph14352@fpt.edu.vn>
2021-12-04 15:21:03 +0700

    Huy-up code

--- client/views/homepage/index.php
+++ client/views/homepage/index.php
@@ -23,32 +23,6 @@
     </div>
     <div class="col-12 col-lg-9 mb-3 mb-lg-0">
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-<<<<<<< Updated upstream
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item ">
-                    <img src="<?= ADMIN_ASSET ?>image/baner1.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="<?= ADMIN_ASSET ?>image/baner6.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item active">
-                    <img src="<?= ADMIN_ASSET ?>image/baner6.jpg" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-=======
             <ul class="sliderimg">
                 <?php foreach ($slide as $item) : ?>
                     <li>
@@ -59,7 +33,6 @@
                 <?php endforeach ?>
             </ul>
 
->>>>>>> Stashed changes
         </div>
 
     </div>
