bb5449a51ba315798e4cf05a71d0018182b8b8e7
longtvph13442 <longtvph13442@fpt.edu.vn>
2021-12-04 21:02:19 +0700

    long-update-paycart

--- client/business/pay_cart.php
+++ client/business/pay_cart.php
@@ -1,8 +1,23 @@
 <?php
+require_once './dao/pay_cart.php';
+function pay()
+{
+  if (isset($_POST["submitt"])) {
+    // thêm thông tin user vào hóa đơn
+    extract($_POST);
+    $sql="insert into hoadon(name,phone,email,address,note) values('$name','$phone','$email','$address','$note')";
+    $hoadon_id= InsertDataAndGetId($sql);
+    //lặp giỏ hàng thêm vào chi tiết hóa đơn
+    foreach($_SESSION["cart"] as $key){
+      $product_id=$key["masp"];
+      $quantyti=$key["soluong"];
+      $price=$key["price"];
+      GetId($hoadon_id,$product_id,$quantyti,$price);
+    }
+    unset($_SESSION["cart"]);
+  }
 
- function pay(){
-   
-   $pay = (isset($_SESSION["cart"])) ? $_SESSION["cart"] : [] ;
-   client_Render("pay_cart/index.php", compact('pay'));
- }
+  $pay = (isset($_SESSION["cart"])) ? $_SESSION["cart"] : [];
+  client_Render("pay_cart/index.php", compact('pay'));
+}
 ?>
