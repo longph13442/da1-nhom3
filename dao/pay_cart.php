60ee041e4585243c2ce06d5012068b40d7228ab3
longtvph13442 <longtvph13442@fpt.edu.vn>
2021-12-06 09:29:21 +0700

    up

--- dao/pay_cart.php
+++ dao/pay_cart.php
@@ -1,9 +1,9 @@
 <?php
 require_once './dao/system_dao.php';
-function pay_cart($name,$phone ,$email , $address, $note,$date)
+function pay_cart($name,$phone ,$email , $address, $note)
 {
-        $sql = "insert into hoadon(name,phone,email,address,note,date) values(?,?,?,?,?,?)";
-        InsertDataAndGetId($sql, $name,$phone ,$email , $address, $note,$date);
+        $sql = "insert into hoadon(name,phone,email,address,note) values(?,?,?,?,?)";
+        InsertDataAndGetId($sql, $name,$phone ,$email , $address, $note);
 }
 function GetId($hoadon_id, $product_id, $quantyti, $price, $total)
 {
